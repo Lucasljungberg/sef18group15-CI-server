c30e09f916e7a2f2a947abc1fb8deb08c91cf72a
Lucasljungberg <lucasljungberg@gmail.com>
2018-01-29 21:29:27 +0100

    feat: Initial commit with Maven structure

---
+++ src/main/java/se/kth/sef18/group15/App.java
@@ -0,0 +1,13 @@
+package se.kth.sef18.group15;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
